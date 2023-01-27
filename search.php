c4e760d2614f68df43b76b206b146719191c6a1e
Kuznetsov Dmitry <dk@infobot.pro>
2023-01-27 20:11:18 +0300

    fix

--- search.php
+++ search.php
@@ -1,14 +1,13 @@
 <?php
 declare(strict_types=1);
 
-function searchNear (Array $arr, int $num) 
+function searchNear (Array $arr, int $num): Array
 {
     $left = -1;
     $right = count($arr);
     $middle = 0;
     while($right - $left > 1) {
-        $middle = floor(($right+$left)/2);
-       
+        $middle = floor(($right + $left) / 2);
         if ($arr[$middle] === $num) {
             return [
                 "index" => $right,
@@ -21,27 +20,27 @@ function searchNear (Array $arr, int $num)
             $left = $middle;
         }
     }
-    $lNum = PHP_INT_MIN;
-    $rNum = PHP_INT_MAX;
-    if ($arr[$left]) {
-        $lNum = $num - $arr[$left];
-    }
-    if ($arr[$right]) {
-        $rNum = $arr[$right] - $num;
-    }
-    if ($lNum < $rNum) {
+    if ($left === -1 || $right === count($arr)) {
         return [
-            "index" => $left,
-            "value" => $arr[$left]
+            "index" => $middle,
+            "value" => $arr[$middle]
         ];
     }
+    if (($arr[$right] - $num) < ($num - $arr[$left])) {
+        return [
+            "index" => $right,
+            "value" => $arr[$right]
+        ];
+    } 
     return [
-        "index" => $right,
-        "value" => $arr[$right]
+        "index" => $left,
+        "value" => $arr[$left]
     ];
 }
 
+print_r(searchNear([1,5,6,8,10,20,40,61], 0));
 print_r(searchNear([1,5,6,8,10,20,40,61], 54));
 print_r(searchNear([1,5,6,8,10,20,40,61], 30));
 print_r(searchNear([1,5,6,8,10,20,40,61], 24));
+print_r(searchNear([1,5,6,8,10,20,40,61], 120));
 
