e82459d6fb165d4015c5b42e630d8b077dd79b29
Kuznetsov Dmitry <dk@infobot.pro>
2023-01-27 20:29:55 +0300

    Fix

--- search.php
+++ search.php
@@ -1,46 +1,54 @@
 <?php
+
 declare(strict_types=1);
 
-function searchNear (Array $arr, int $num): Array
+namespace Dk\Test;
+
+class NearElement
+{
+    public $value;
+    public $index;
+    function __construct(int $index, int $value)
+    {
+        $this->index = $index;
+        $this->value = $value;
+    }
+}
+
+function searchNear(array $arr, int $num): NearElement
 {
     $left = -1;
     $right = count($arr);
     $middle = 0;
-    while($right - $left > 1) {
-        $middle = floor(($right + $left) / 2);
+
+    while ($right - $left > 1) {
+        $middle = (int) floor(($right + $left) / 2);
+
         if ($arr[$middle] === $num) {
-            return [
-                "index" => $right,
-                "value" => $arr[$middle]
-            ];
+            return new NearElement($middle, $arr[$middle]);
         }
+
         if ($num < $arr[$middle]) {
             $right = $middle;
         } else {
             $left = $middle;
         }
     }
+
     if ($left === -1 || $right === count($arr)) {
-        return [
-            "index" => $middle,
-            "value" => $arr[$middle]
-        ];
+        return new NearElement($middle, $arr[$middle]);
     }
+
     if (($arr[$right] - $num) < ($num - $arr[$left])) {
-        return [
-            "index" => $right,
-            "value" => $arr[$right]
-        ];
-    } 
-    return [
-        "index" => $left,
-        "value" => $arr[$left]
-    ];
+        return new NearElement($right, $arr[$right]);
+    }
+
+    return new NearElement($left, $arr[$left]);
 }
 
-print_r(searchNear([1,5,6,8,10,20,40,61], 0));
-print_r(searchNear([1,5,6,8,10,20,40,61], 54));
-print_r(searchNear([1,5,6,8,10,20,40,61], 30));
-print_r(searchNear([1,5,6,8,10,20,40,61], 24));
-print_r(searchNear([1,5,6,8,10,20,40,61], 120));
-
+print_r(searchNear([1, 5, 6, 8, 10, 20, 40, 61], 0));
+print_r(searchNear([1, 5, 6, 8, 10, 20, 40, 61], 8));
+print_r(searchNear([1, 5, 6, 8, 10, 20, 40, 61], 54));
+print_r(searchNear([1, 5, 6, 8, 10, 20, 40, 61], 30));
+print_r(searchNear([1, 5, 6, 8, 10, 20, 40, 61], 24));
+print_r(searchNear([1, 5, 6, 8, 10, 20, 40, 61], 120));
